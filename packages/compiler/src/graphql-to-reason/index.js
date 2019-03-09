46444281e7bba1a0bd006e49d567ef82cefab573
sainthkh <sainthkh@naver.com>
2019-03-09 12:46:56 +0900

    Now, we can generate array codes.

--- packages/compiler/src/graphql-to-reason/index.js
+++ packages/compiler/src/graphql-to-reason/index.js
@@ -1,10 +1,8 @@
-const {schemaToReason} = require('./schema');
 const {queryToReason} = require('./client');
 const {createTypeMap} = require('./typemap');
 const {findTags} = require('./tagFinder');
 
 module.exports = {
-  schemaToReason,
   queryToReason,
   createTypeMap,
   findTags,
